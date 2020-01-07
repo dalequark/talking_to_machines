587217de4abf3d3b41939807576fff305a924f75
Dale Markowitz <damarkowitz@gmail.com>
2020-01-07 23:34:52 +0000

    stashing

--- aiy/src/buttonled.js
+++ aiy/src/buttonled.js
@@ -2,21 +2,20 @@ const BUTTON_PIN = 23;
 
 const Gpio = require('onoff').Gpio;
 const {Leds, Color} = require('./Leds');
-const led = new Gpio(LED_PIN, 'out');
 const button = new Gpio(BUTTON_PIN, 'in', 'rising', {debounceTimeout: 10});
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
   }
 
-const colors = [Colors.RED, Colors.BLUE, Colors.CYAN, Colors.YELLOW, Colors.WHITE, Colors.PURPLE];
+const colors = [Color.RED, Color.BLUE, Color.CYAN, Color.YELLOW, Color.WHITE, Color.PURPLE];
+
+const leds = new Leds();
+
+leds.reset();
 
 button.watch((err, value) => {
 	console.log("Got button press");
-	leds.update(color[getRandomInt(colors.length)]);
+	leds.update(colors[getRandomInt(colors.length)]);
 });
-
-const leds = Leds();
-
-leds.reset();
 
