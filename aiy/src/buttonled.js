37f7761abd369b2198a256ae93dab33e386b735f
Dale Markowitz <dale.markowitz@mlwrite.com>
2020-01-07 16:44:48 -0600

    testing on rapi

--- aiy/src/buttonled.js
+++ aiy/src/buttonled.js
@@ -1,57 +1,22 @@
 const BUTTON_PIN = 23;
-const LED_PIN = 25;
 
-const fs = require('fs');
 const Gpio = require('onoff').Gpio;
+const {Leds, Color} = require('./Leds');
 const led = new Gpio(LED_PIN, 'out');
 const button = new Gpio(BUTTON_PIN, 'in', 'rising', {debounceTimeout: 10});
-const path = require('path');
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+  }
+
+const colors = [Colors.RED, Colors.BLUE, Colors.CYAN, Colors.YELLOW, Colors.WHITE, Colors.PURPLE];
 
 button.watch((err, value) => {
 	console.log("Got button press");
-	led.writeSync(value);
-	});
+	leds.update(color[getRandomInt(colors.length)]);
+});
 
+const leds = Leds();
 
-const DEVICE_PATH = '/sys/class/leds/ktd202x:led1/device/';
+leds.reset();
 
-function device_file(prop) {
-	return path.join(DEVICE_PATH, prop);
-}
-
-function write(path, data) {
-	fs.writeFileSync(path, data);	
-}
-
-function update(channels) {
-	let command = '';
-	for (let i = 0; i < channels.length; i++) {
-		const channel = channels[i];
-		if (channel["brightness"]) {
-			command += `led${i+1}=${channel["brightness"]};`;
-		}
-		if (channel["state"]) {
-			command += `ch${i}_enabled=${channel["state"]};`;
-		}
-	}
-	if (command) {
-		write(device_file("registers"), command);
-	}
-}
-
-function rgb_off() {
-	
-}
-
-function reset() {
-	write(device_file("reset"), 1);
-}
-
-let channels = [
-	{"brightness" : 0, "state" : 0},
-	{"brightness" : 0, "state" : 0},
-	{"brightness" : 0xFF, "state" : 1}
-];
-
-reset(channels);
-update(channels);
