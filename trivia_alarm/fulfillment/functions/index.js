4cb57849085bab864f8979a60b0fbd04842656d2
Dale Markowitz <dale.markowitz@mlwrite.com>
2020-02-24 14:29:21 -0600

    extensive commenting and removed some print statements

--- trivia_alarm/fulfillment/functions/index.js
+++ trivia_alarm/fulfillment/functions/index.js
@@ -27,14 +27,17 @@ process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
+// Set this to whatever message you want to play when
+// the trivia game begins.
 const WELCOME_MESSAGE = `Good morning! Good morning! Wake up! 
 It\'s time to answer some trivia questions.`;
 
+// Total number of questions until the user "wins"
 const NUM_QUESTIONS = 3;
 
-// TODO: Add users. For now, we'll use a dummy id.
-const USER_ID = '3HGBBSXFlwArhH5MbvHb';
-
+// Given an answer, extracts the number. For example,
+// given "The answer is 15", this fn returns the number
+// 15 or null, if no number is found.
 function extractNumber(answer) {
   const result = answer.match(/\d+/);
   return result ? result[0] : null;
@@ -45,20 +48,25 @@ functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({request, response});
 
 
+  // Returns trivia question in increasing hardness as the
+  // game goes on.
   function getQuestion() {
+    // Get the # of questions the user has answered corrrectly already
+    // from the context.
     const questionNum = agent.context.get('quiz_data') ? agent.context.get('quiz_data').questions_correct + 1 : 1;
-    // Start the first third questions easy, then medium, then difficult
+    
+    / Start the first third questions easy, then medium, then difficult
     if (questionNum / NUM_QUESTIONS <= 1/3)  return trivia.getEasyQuestion();
     if (questionNum / NUM_QUESTIONS <= 2/3)  return trivia.getMediumQuestion();
     return trivia.getHardQuestion();
   }
 
-  
+  // We store information about the current trivia question, it's answer,
+  // and the number of questions answered correctly so far 
+  // in the Agent's context. This function takes a question
+  // and stoers it in the context.
   function setQuestionContext(question) {
     const oldContext = agent.context.get('quiz_data');
-    console.log('Setting question context. Old context was ', oldContext);
-    console.log(`Setting question context with new question ${question.question}`);
-    console.log(`Setting question context with questions correct ${ oldContext ? oldContext.parameters.questions_correct : 0}`);
     const questionsCorrect =
         oldContext ? oldContext.parameters.questions_correct : 0;
     agent.context.set({
@@ -72,29 +80,42 @@ functions.https.onRequest((request, response) => {
     });
   }
 
+  // Intent handler responsible for asking trivia questions.
+  // This Intent can be triggered through code or in a 
+  // fallback from one of the other Intent handlers.
   async function askQuestion(agent) {
-    console.log('Hello from Ask Question');
 
+    // Because askQuestion can be triggered
+    // from other Intent handlers, we use lastState to
+    // keep track of how we got here. Did the user
+    // just get the last question wrong? Right? Did they
+    // try to snooze?
     const lastState = agent.parameters.lastState;
     console.log(`Last state was ${lastState}`);
 
+    // If we're just beginning the quiz game, there's no
+    // prior state.
     if (!lastState) {
-      // Set up the first question
+      // Set up the first question. 
+      
+      // No context should exist yet, but if it does,
+      // delete it.
       agent.context.delete('quiz_data');
       const question = getQuestion();
       setQuestionContext(question);
-      console.log('Adding message to agent');
       agent.add(`${WELCOME_MESSAGE}
-      Your first question is ${question.question}`);
+        Your first question is ${question.question}`);
       return;
     }
 
+    // If the quiz game isn't just starting, we should have
+    // already asked a question which is stored in context.
     const context = agent.context.get('quiz_data');
-    console.log('In ask question, got context ', context);
 
     if (context.parameters.questions_correct == NUM_QUESTIONS) {
+      // If the user answered NUM_QUESTIONS questions correctly,
+      // we're done! Trigger the Quiz Done intent.
       agent.context.set('trivia-done');
-      agent.add('Some dummy text');
       agent.setFollowupEvent({
         'name': 'quiz-done',
         'languageCode': 'en',
@@ -103,11 +124,13 @@ functions.https.onRequest((request, response) => {
       return;
     }
 
+    // If the user tries to snooze, don't let them!
     if (lastState == 'Snooze') {
       agent.add(`No time for snoozing! ${context.parameters.question}`);
       return;
     }
 
+    // If the user doesn't know the answer, get them a new question
     if (lastState == 'Next Question') {
       const question = getQuestion();
       setQuestionContext(question);
@@ -116,17 +139,22 @@ functions.https.onRequest((request, response) => {
       return;
     }
 
+    // If the user doesn't respond with a number, ask the question
+    // again and remind them need to answer with a number.
     if (lastState == 'No Number') {
       agent.add(`What's that? Remember to answer with a number. 
       ${context.parameters.question}`);
       return;
     }
 
+    // If the user gives the wrong answer, let them try again.
     if (lastState == 'Wrong Answer') {
       agent.add(`Sorry, wrong answer. ${context.parameters.question}`);
       return;
     }
 
+    // If the user gives ther ight answer, congratulate them and pull
+    // a new question.
     if (lastState == 'Right Answer') {
       const question = getQuestion();
       setQuestionContext(question);
@@ -137,10 +165,11 @@ functions.https.onRequest((request, response) => {
     agent.add('Got to question answer in a weird way. We should never get here!');
   }
 
+  // Handler for the Snooze Intent
   function snooze(agent) {
     console.log('Hello from Snooze');
-    // If user tries to snooze, pop them back to question-1.
-    agent.add('Some dummy text');
+    // If user tries to snooze, ignore their request and ask
+    // the current trivia question again
     agent.setFollowupEvent({
       'name': 'ask-question',
       'parameters': {
@@ -151,9 +180,8 @@ functions.https.onRequest((request, response) => {
     );
   }
 
+  // Handler from the Next Question intent
   function nextQuestion(agent) {
-    console.log('Hello from Next Question');
-    agent.add('Some dummy text');
     agent.setFollowupEvent({
       'name': 'ask-question',
       'parameters': {
@@ -164,26 +192,28 @@ functions.https.onRequest((request, response) => {
     );
   }
 
+  // Handler for the answerQuestion intent. Since we made this
+  // Intent a fallback, this function will run whenever no other
+  // Intent (Snooze, Later, etc) was matched.
   function answerQuestion(agent) {
-    console.log(`In question answer with user query ${agent.query}`);
 
     const userAnswer = extractNumber(agent.query);
 
-    console.log(`Got user answer ${userAnswer}`);
-
+    // Placeholder. We should always overwrite this value.
     let lastState = 'Mystery State';
 
     const context = agent.context.get('quiz_data');
 
-    console.log('Got context ', context);
-
+    // Set state to No Number when the user didn't answer
+    // with a number
     if (!userAnswer) {
       lastState = 'No Number';
-      console.log('Got no user answer');
     } else if (context.parameters.answer == userAnswer) {
       console.log(`User got the right answer`);
 
       lastState = 'Right Answer';
+      // Update the number of questions the user got correct
+      // in the agent context.
       agent.context.set({
         name: 'quiz_data',
         lifespan: 99,
@@ -197,8 +227,9 @@ functions.https.onRequest((request, response) => {
       console.log(`User gave the wrong answer: ${userAnswer} vs correct ${context.parameters.answer}`);
       lastState = 'Wrong Answer';
     }
-    console.log(`Setting follow up event with last state ${lastState}`);
-    agent.add('Some dummy text');
+
+    // After identifying which case we're in above, invoke
+    // the Ask Question Intent.
     agent.setFollowupEvent({
       'name': 'ask-question',
       'parameters': {
