f8754da20c41510361bb3057d4cec15999791083
Dale Markowitz <dalequark@google.com>
2020-04-08 13:16:29 -0500

    discord karma bot

--- discord_karma/perspective.js
+++ discord_karma/perspective.js
@@ -14,34 +14,61 @@
  * limitations under the License.
  */
 
- /* Example usage of some features of the Perspective API */
-var googleapis = require('googleapis');
+/* Example usage of some features of the Perspective API */
+const googleapis = require('googleapis');
 
 require('dotenv').config();
 
 DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
 
-// Some attributes to try out
-attributes = ["TOXICITY", "SEVERE_TOXICITY", "IDENTITY_ATTACK", "INSULT",
-"PROFANITY", "THREAT", "SEXUALLY_EXPLICIT", "FLIRTATION", "SPAM", "ATTACK_ON_AUTHOR", 
-"ATTACK_ON_COMMENTER", "INCOHERENT", "INFLAMMATORY", "OBSCENE", "SPAM", "UNSUBSTANTIAL"];
+// Some supported attributes
+// attributes = ["TOXICITY", "SEVERE_TOXICITY", "IDENTITY_ATTACK", "INSULT",
+// "PROFANITY", "THREAT", "SEXUALLY_EXPLICIT", "FLIRTATION", "SPAM",
+// "ATTACK_ON_AUTHOR", "ATTACK_ON_COMMENTER", "INCOHERENT",
+// "INFLAMMATORY", "OBSCENE", "SPAM", "UNSUBSTANTIAL"];
 
+// Set your own thresholds for when to trigger a response
+const attributeThresholds = {
+  'INSULT': 0.75,
+  'TOXICITY': 0.75,
+  'SPAM': 0.75,
+  'INCOHERENT': 0.75,
+  'FLIRTATION': 0.75,
+};
+
+/**
+ * Analyze attributes in a block of text
+ * @param {string} text - text to analyze
+ * @return {json} res - analyzed atttributes
+ */
 async function analyzeText(text) {
-    let analyzer = new googleapis.commentanalyzer_v1alpha1.Commentanalyzer();
+  const analyzer = new googleapis.commentanalyzer_v1alpha1.Commentanalyzer();
 
-    let req = { comment: {text: text}, requestedAttributes: {'INCOHERENT': {}}};
-    
-    let res = await analyzer.comments.analyze({
-        key: process.env.PERSPECTIVE_API_KEY, 
-        resource: req}
-    )
+  // This is the format the API expects
+  const requestedAttributes = {};
+  for (const key in attributeThresholds) {
+    requestedAttributes[key] = {};
+  }
 
-    return res['data']['attributeScores'];
+  const req = {
+    comment: {text: text},
+    languages: ['en'],
+    requestedAttributes: requestedAttributes,
+  };
+
+  const res = await analyzer.comments.analyze({
+    key: process.env.PERSPECTIVE_API_KEY,
+    resource: req},
+  );
+
+  data = {};
+
+  for (const key in res['data']['attributeScores']) {
+    data[key] =
+        res['data']['attributeScores'][key]['summaryScore']['value'] >
+        attributeThresholds[key];
+  }
+  return data;
 }
 
-async function main() {
-    let score = await analyzeText("Gobbledy Gobbledy ding dong");
-    console.log(score);
-}
-
-main();
+module.exports.analyzeText = analyzeText;
